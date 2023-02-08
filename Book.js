fb8b6e673774107173af503b6a50cffb10506d84
abhijeetpode <106150468+abhijeet1122333@users.noreply.github.com>
2023-02-08 01:09:17 -0800

    Frontend

--- Book.js
+++ Book.js
@@ -1,27 +1,29 @@
-const mongoose = require('mongoose');
+import React from 'react';
+import { deleteBook } from '../../redux/actions/books/bookActions';
+import { useDispatch } from 'react-redux';
 
-const BookSchema = new mongoose.Schema(
-  {
-    category: {
-      type: String,
-      required: true,
-    },
-    author: {
-      type: String,
-      required: true,
-    },
-    title: {
-      type: String,
-    },
-    createdBy: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
-  },
-  { timestamps: true }
-);
+const Book = () => {
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <div class='card mb-3' style={{ maxWidth: '540px' }}>
+        <div class='row no-gutters'>
+          <div class='col-md-4'>
+            <img src='...' class='card-img' alt='...' />
+          </div>
+          <div class='col-md-8'>
+            <div class='card-body'>
+              <h5 class='card-title'>Book</h5>
+              <p class='card-text'>Book detail</p>
+              <p class='card-text'>
+                <small class='text-muted'>Last updated 3 mins ago</small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-const Book = mongoose.model('Book', BookSchema);
-
-module.exports = Book;
+export default Book;
